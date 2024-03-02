2a98f911e6cbe8b0d319dadff80a23aac29296f3
xela6134 <leol3o05@gmail.com>
2024-03-02 19:14:13 +1100

    90% finished homepage

--- src/App.js
+++ src/App.js
@@ -21,7 +21,39 @@ export const restaurantsBackend = [
         description:
             'Guzman y Gomez (GYG) is a fast-casual Mexican restaurant chain that originated in Australia and has expanded globally. Specializing in fresh and authentic Mexican cuisine, GYG offers a diverse menu featuring tacos, burritos, nachos, and other traditional Mexican dishes made with high-quality ingredients. The chain is known for its commitment to sustainability and ethically sourced produce, as well as its vibrant and welcoming atmosphere.',
         route: '/1',
-    }
+    },
+    {
+        heading: 'Mcdonalds',
+        content: 'Gods Palace',
+        logo: 'https://upload.wikimedia.org/wikipedia/en/thumb/7/73/Guzman_y_Gomez_logo.svg/1200px-Guzman_y_Gomez_logo.svg.png',
+        description:
+            'Guzman y Gomez (GYG) is a fast-casual Mexican restaurant chain that originated in Australia and has expanded globally. Specializing in fresh and authentic Mexican cuisine, GYG offers a diverse menu featuring tacos, burritos, nachos, and other traditional Mexican dishes made with high-quality ingredients. The chain is known for its commitment to sustainability and ethically sourced produce, as well as its vibrant and welcoming atmosphere.',
+        route: '/1',
+    },
+    {
+        heading: 'Mcdonalds',
+        content: 'Gods Palace',
+        logo: 'https://upload.wikimedia.org/wikipedia/en/thumb/7/73/Guzman_y_Gomez_logo.svg/1200px-Guzman_y_Gomez_logo.svg.png',
+        description:
+            'Guzman y Gomez (GYG) is a fast-casual Mexican restaurant chain that originated in Australia and has expanded globally. Specializing in fresh and authentic Mexican cuisine, GYG offers a diverse menu featuring tacos, burritos, nachos, and other traditional Mexican dishes made with high-quality ingredients. The chain is known for its commitment to sustainability and ethically sourced produce, as well as its vibrant and welcoming atmosphere.',
+        route: '/1',
+    },
+    {
+        heading: 'Mcdonalds',
+        content: 'Gods Palace',
+        logo: 'https://upload.wikimedia.org/wikipedia/en/thumb/7/73/Guzman_y_Gomez_logo.svg/1200px-Guzman_y_Gomez_logo.svg.png',
+        description:
+            'Guzman y Gomez (GYG) is a fast-casual Mexican restaurant chain that originated in Australia and has expanded globally. Specializing in fresh and authentic Mexican cuisine, GYG offers a diverse menu featuring tacos, burritos, nachos, and other traditional Mexican dishes made with high-quality ingredients. The chain is known for its commitment to sustainability and ethically sourced produce, as well as its vibrant and welcoming atmosphere.',
+        route: '/1',
+    },
+    {
+        heading: 'Mcdonalds',
+        content: 'Gods Palace',
+        logo: 'https://upload.wikimedia.org/wikipedia/en/thumb/7/73/Guzman_y_Gomez_logo.svg/1200px-Guzman_y_Gomez_logo.svg.png',
+        description:
+            'Guzman y Gomez (GYG) is a fast-casual Mexican restaurant chain that originated in Australia and has expanded globally. Specializing in fresh and authentic Mexican cuisine, GYG offers a diverse menu featuring tacos, burritos, nachos, and other traditional Mexican dishes made with high-quality ingredients. The chain is known for its commitment to sustainability and ethically sourced produce, as well as its vibrant and welcoming atmosphere.',
+        route: '/1',
+    },
 ];
 
 const App = () => (
--- src/Homepage.js
+++ src/Homepage.js
@@ -2,39 +2,51 @@ import Container from './components/RoundedContainer.js';
 import Zoom from '@mui/material/Zoom';
 import './Homepage.scss';
 import { restaurantsBackend } from './App.js';
+import unswLogo from './unsw-logo.png';
 
 const Homepage = () => (
-    <div>
-        <h1>Homepage</h1>
-        <div className="rest-container">
-            {restaurantsBackend.map((element, index) => {
-                return (
-                    <Zoom
-                        in={true}
-                        style={{ transitionDelay: `${100 * index}ms` }}
-                    >
-                        <div
-                            style={{
-                                background: `${
-                                    element?.colour ? element?.colour : ''
-                                }`,
-                            }}
+    <div style={{ padding: '48px' }}>
+        <div className='title'>
+            <h1 id='title-text' >Food Reviews for </h1>
+            <img id='unsw-logo' alt='UNSW Logo' src={unswLogo} />
+        </div>
+        <div className='bottom-container' style={{ display: 'inline-flex', width: '100%', gap: '32px', }}>
+            <div className='description'>
+                <p>Welcome to UNSW Eats, the ultimate guide to dining around the University of New South Wales! Our platform is designed for students, by students, to help you make the most of your culinary adventures. Whether you're looking for a cozy café to study in, a quick bite between classes, or a hidden gem for a special occasion, UNSW Eats has you covered.</p>
+                <p>Join our community of food enthusiasts to discover and share your favorite dining spots. Upload your own reviews, photos, and recommendations to help your fellow students find the perfect meal. From trendy eateries to classic favorites, UNSW Eats is your go-to resource for all things food on campus.</p>
+                <p>Join us in celebrating the vibrant food scene around UNSW. Let's eat, explore, and share the joy of good food together!</p>
+                <p>- The UNSW Food Dev Team</p>
+            </div>
+            <div className="rest-container">
+                {restaurantsBackend.map((element, index) => {
+                    return (
+                        <Zoom
+                            in={true}
+                            style={{ transitionDelay: `${100 * index}ms` }}
                         >
-                            <Container
-                                heading={element.heading}
-                                content={<>{element.content} </>}
-                                logo={element?.logo}
-                                description={element?.description}
+                            <div
                                 style={{
-                                    width: '300px',
-                                    height: '120px',
+                                    background: `${
+                                        element?.colour ? element?.colour : ''
+                                    }`,
                                 }}
-                                route={element?.route}
-                            />
-                        </div>
-                    </Zoom>
-                );
-            })}
+                            >
+                                <Container
+                                    heading={element.heading}
+                                    content={<>{element.content} </>}
+                                    logo={element?.logo}
+                                    description={element?.description}
+                                    style={{
+                                        width: '320px',
+                                        height: '120px'
+                                    }}
+                                    route={element?.route}
+                                />
+                            </div>
+                        </Zoom>
+                    );
+                })}
+            </div>
         </div>
     </div>
 );
